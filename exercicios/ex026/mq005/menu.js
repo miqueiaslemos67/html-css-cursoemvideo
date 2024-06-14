01d1cadb7fd8582532c1aa50a3d11e82f7058cde
miqueiaslemos67 <155577371+miqueiaslemos67@users.noreply.github.com>
2024-06-14 11:38:21 -0300

    Ex026 Menu Hamburguer.

---
+++ exercicios/ex026/mq005/menu.js
@@ -0,0 +1,9 @@
+
+// função em JS de sidebar (menu de Hamburguer)
+function clickMenu() {
+  if(itens.style.display == 'block') {
+    itens.style.display = 'none'
+  } else {
+    itens.style.display = 'block'
+  }
+}
