6061d1df62ade569b52dbbd90e382cd9995cc4a1
miqueiaslemos67 <155577371+miqueiaslemos67@users.noreply.github.com>
2024-06-16 14:34:40 -0300

    Aula finalizada de Media Query

--- exercicios/ex026/mq005/menu.js
+++ exercicios/ex026/mq005/menu.js
@@ -1,3 +1,14 @@
+// função par o menu seja forçado a aparecer em telas grandes
+
+function mudouTamanho() {
+  if (window.innerWidth >= 768) {
+    itens.style.display = 'block'
+  } else {
+    itens.style.display = 'none'
+  }
+}
+
+
 
 // função em JS de sidebar (menu de Hamburguer)
 function clickMenu() {
